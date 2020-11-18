f07b60aecf7688f1fd2cc4efadc9bf1b0991a423
Bryan <bryan.dov.bergman@gmail.com>
2020-11-18 22:40:24 +0200

    Initial commit

---
+++ app/src/main/java/com/example/menu_demo/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.menu_demo;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch(item.getItemId()){
+            case R.id.settings:
+                Log.i("Item Selected", "settings");
+                Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.help:
+                Log.i("Item Selected", "Help");
+                Toast.makeText(this, "help", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
